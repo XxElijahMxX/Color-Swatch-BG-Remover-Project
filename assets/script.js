73331c91ebad7065050a8ce64f231d4fed06b6d3
Mugen <melton.sheridan@gmail.com>
2022-04-13 18:56:13 -0400

    added new html for secondary api Horoscope

--- assets/script.js
+++ assets/script.js
@@ -23,22 +23,39 @@ function getData() {
         return response.json();
     }).then(displayWeather);    
 }
-
+// this function displays the information on the page and writes it as HTML elements
 function displayWeather (response) {
     console.log(response);
+    // the returns an invalid city response if user inputs a non city search
     if (response.cod === "404") {
         const error = document.querySelector(".error");
         error.textContent = "This is not a valid city";
         search.value = "";
     } else {
+        // this defines a city variable and writes the city and country on the page
         const city = document.querySelector(".city");
         city.innerText = `${response.name}, ${response.sys.country}`;
 
+        // this defines the temperature variable and writes the fetched parameer for the temp on the page in celsius
         const temp = document.querySelector(".temp");
         temp.innerHTML = `Temp: ${Math.round(response.main.temp)} <span>°</span>C`;
 
+        // this defines the feel like variable and writes the parameter for feels like to the page in celsius
         const feelsLike =document.querySelector(".feels-like");
         feelsLike.innerHTML = ` Feels Like: ${Math.round(response.main.feels_like)}<span>°</span>C`;
         
     }
 }
+
+const options = {
+	method: 'POST',
+	headers: {
+		'X-RapidAPI-Host': 'sameer-kumar-aztro-v1.p.rapidapi.com',
+		'X-RapidAPI-Key': '8103907254mshd54dafee3143633p1ec4e7jsnb927eb9caaad'
+	}
+};
+
+fetch('https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=aquarius&day=today', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
